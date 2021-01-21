eb61ea706b38b7a46ba996bde9ae5387a32f89bc
The Coden <75643469+thecoden@users.noreply.github.com>
2021-01-21 08:20:49 -0800

    ktlint : FileUtils : Implement native Klob support

--- ktlint/src/main/kotlin/com/pinterest/ktlint/internal/FileUtils.kt
+++ ktlint/src/main/kotlin/com/pinterest/ktlint/internal/FileUtils.kt
@@ -1,37 +1,28 @@
 package com.pinterest.ktlint.internal
 
-import com.github.shyiko.klob.Glob
 import com.pinterest.ktlint.core.KtLint
 import com.pinterest.ktlint.core.LintError
 import com.pinterest.ktlint.core.RuleSet
 import java.io.File
+import java.nio.file.FileSystems
 import java.nio.file.Path
-import java.nio.file.Paths
+import java.nio.file.PathMatcher
 import kotlin.system.exitProcess
 
 internal val workDir: String = File(".").canonicalPath
 
+/**
+ * For a list of Strings that represent file matching patterns,
+ * returns a [Sequence] of Files that match the patterns.
+ */
 internal fun List<String>.fileSequence(): Sequence<File> {
-    val kotlinFiles: Sequence<Path> = if (isEmpty()) {
-        Glob.from("**/*.kt", "**/*.kts")
-            .iterate(
-                Paths.get(workDir),
-                Glob.IterationOption.SKIP_HIDDEN
-            )
-            .asSequence()
+    val patterns = if (isEmpty()) {
+       listOf("**/*.kt", "**/*.kts")
     } else {
-        // Converting List<String> to Array<String> and passing it to Glob.from(patterns) skips some files
-        // See https://github.com/pinterest/ktlint/issues/942
-        map {
-            Glob.from(expandTilde(it))
-                .iterate(Paths.get(workDir))
-        }
-            .asSequence()
-            .flatMap { it.asSequence() }
+        map(::expandTilde).toList()
     }
 
-    return kotlinFiles
-        .map(Path::toFile)
+    return FileMatcher(patterns).getFiles(File(workDir))
 }
 
 /**
@@ -50,7 +41,7 @@ internal fun JarFiles.toFilesURIList() = map {
 
 // a complete solution would be to implement https://www.gnu.org/software/bash/manual/html_node/Tilde-Expansion.html
 // this implementation takes care only of the most commonly used case (~/)
-private fun expandTilde(path: String): String = path.replaceFirst(Regex("^~"), System.getProperty("user.home"))
+private fun expandTilde(path: String): String = path.replaceFirst(Regex("^(!)?~"), "$1" + System.getProperty("user.home"))
 
 internal fun File.location(
     relative: Boolean
@@ -108,3 +99,70 @@ internal fun formatFile(
             debug = debug
         )
     )
+
+/**
+ * Given a list of Patterns, provides Path matching
+ * and convenient searching for files that match.
+ */
+internal class FileMatcher(patterns: List<String>) {
+
+    /**
+     * A list of [Triple] values, each representing an individual pattern
+     * and the important details of it.
+     * Each triple contains:
+     * - isNegated : Whether the pattern is used to exclude files from matching.
+     * - PathMatcher : The [PathMatcher] resulting from compiling the pattern.
+     * - The original pattern string, for debugging / logging purposes.
+     */
+    private val patterns = patterns.map { pattern ->
+        val patternType = if (pattern.startsWith("regex:")) {
+            "regex"
+        } else {
+            "glob"
+        }
+
+        // Remove any pattern type prefix and determine if the pattern is negated.
+        val isNegated:Boolean
+        val patternOnly = pattern.removePrefix("$patternType:").let {
+            isNegated = !it.startsWith("!")
+            it.removePrefix("!")
+        }
+
+        // Create a Triple of isNegated, the resulting PathMatcher, and the original pattern.
+        Triple<Boolean, PathMatcher, String>(isNegated, FileSystems.getDefault().getPathMatcher("$patternType:$patternOnly"), pattern)
+    }
+
+    /**
+     * Determine if the given [path] matches the list of file patterns.
+     * Be sure the path cardinality (relative or absolute), matches the
+     * cardinality of the patterns you are attempting to match.
+     */
+    fun matches(path: Path): Boolean {
+        var isIncluded = false
+
+        for (pattern in patterns) {
+            if (isIncluded) {
+                if (!pattern.first) {
+                    isIncluded = !pattern.second.matches(path)
+                }
+            } else {
+                if (pattern.first) {
+                    isIncluded = pattern.second.matches(path)
+                }
+            }
+        }
+
+        return isIncluded
+    }
+
+    /**
+     * Get all files that match the given patterns in a directory tree.
+     */
+    fun getFiles(baseDirectory: File) = baseDirectory.walkTopDown().filter { file ->
+        if (file.isFile) {
+            matches(baseDirectory.toPath().relativize(file.toPath()))
+        } else {
+            false
+        }
+    }
+}
